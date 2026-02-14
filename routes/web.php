b6379bfbd61bce7571ff88718b0f2ee9493fae74
aloisius <aloisius@radyalabs.com>
2026-02-14 08:02:37 +0700

    try to debug

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,89 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MovieController;
 use App\Http\Controllers\LanguageController;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\File;
+
+// route debugs
+Route::get('/debug-assets', function() {
+    $data = [
+        'environment' => app()->environment(),
+        'app_url' => config('app.url'),
+        'asset_url' => asset(''),
+        'public_path' => public_path(),
+        'base_path' => base_path(),
+    ];
+
+    // Cek folder build
+    $buildPath = public_path('build');
+    $data['build_exists'] = file_exists($buildPath);
+
+    if (file_exists($buildPath)) {
+        $data['build_contents'] = array_diff(scandir($buildPath), ['.', '..']);
+
+        // Cek manifest
+        $manifestPath = $buildPath . '/manifest.json';
+        if (file_exists($manifestPath)) {
+            $manifest = json_decode(file_get_contents($manifestPath), true);
+            $data['manifest'] = $manifest;
+
+            // Test asset URLs
+            if (isset($manifest['resources/css/app.css'])) {
+                $cssFile = $manifest['resources/css/app.css']['file'];
+                $data['css_file'] = $cssFile;
+                $data['css_exists'] = file_exists(public_path($cssFile));
+                $data['css_url'] = asset($cssFile);
+            }
+
+            if (isset($manifest['resources/js/app.js'])) {
+                $jsFile = $manifest['resources/js/app.js']['file'];
+                $data['js_file'] = $jsFile;
+                $data['js_exists'] = file_exists(public_path($jsFile));
+                $data['js_url'] = asset($jsFile);
+            }
+        }
+
+        // Cek folder assets
+        $assetsPath = $buildPath . '/assets';
+        if (file_exists($assetsPath)) {
+            $data['assets_contents'] = array_diff(scandir($assetsPath), ['.', '..']);
+        }
+    }
+
+    // Test langsung akses file
+    $testFiles = [
+        'manifest' => '/build/manifest.json',
+        'css' => '/build/assets/app-D7in9Jx8.css',
+        'js' => '/build/assets/app-Dz35q6s9.js',
+    ];
+
+    foreach ($testFiles as $key => $file) {
+        $fullPath = public_path($file);
+        $data['file_check'][$key] = [
+            'path' => $file,
+            'exists' => file_exists($fullPath),
+            'size' => file_exists($fullPath) ? filesize($fullPath) : 0,
+            'url' => asset($file),
+        ];
+    }
+
+    return response()->json($data);
+});
+
+Route::get('/test-css', function() {
+    $cssPath = public_path('build/assets/app-D7in9Jx8.css');
+    if (file_exists($cssPath)) {
+        return response(file_get_contents($cssPath))->header('Content-Type', 'text/css');
+    }
+    return 'CSS not found';
+});
+
+Route::get('/test-js', function() {
+    $jsPath = public_path('build/assets/app-Dz35q6s9.js');
+    if (file_exists($jsPath)) {
+        return response(file_get_contents($jsPath))->header('Content-Type', 'application/javascript');
+    }
+    return 'JS not found';
+});
 
 
 // Language Switch
